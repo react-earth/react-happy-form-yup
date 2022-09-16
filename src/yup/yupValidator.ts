a8e3c0d068606c7d00ff176185c1d2a2e66df066
7zf001 <zhifeng.tan@compass.com>
2022-09-16 16:57:58 +0800

    Feat

---
+++ src/yup/yupValidator.ts
@@ -0,0 +1,39 @@
+import * as Yup from "yup";
+import type Lazy from "yup/lib/Lazy";
+
+type Options<T extends Yup.AnyObjectSchema | Lazy<any>> = Parameters<
+  T["validate"]
+>[1];
+
+export type Validator = <T extends Yup.AnyObjectSchema | Lazy<any>>(
+  schema: T,
+  schemaOptions?: Options<T>,
+  validateOptions?: { mode?: "async" | "sync"; rawValues?: boolean }
+) => (values: any) => Promise<{ values: any; errors: Record<string, string> }>;
+
+export const yupValidator: Validator =
+  (schema, schemaOptions = {}, validateOptions = {}) =>
+  async (values) => {
+    try {
+      const result = await schema[
+        validateOptions.mode === "sync" ? "validateSync" : "validate"
+      ](values, Object.assign({ abortEarly: false }, schemaOptions));
+
+      return {
+        values: result,
+        errors: {},
+      };
+    } catch (e: any) {
+      if (!e.inner) {
+        throw e;
+      }
+
+      return {
+        values: {},
+        errors: e.inner.reduce((error: any, item: any) => {
+          error[item.path] = item.message;
+          return error;
+        }, {}),
+      };
+    }
+  };
